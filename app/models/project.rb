4c43e0a9e850d4be254558acdd06bf787046833c
vagrant <vagrant@ironhack>
2014-07-28 11:18:08 +0000

    initial commit

---
+++ app/models/project.rb
@@ -0,0 +1,9 @@
+class Project < ActiveRecord::Base
+end
+
+# Lo de arriba es lo mismo que ésto
+# class Project < ActiveRecord::Base
+# 	set_table_name "projects"
+# end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get "/" => "site#home"
   get "/contact" => "site#email"
-  get "/call" => "site#call"
 # / aka URL
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
