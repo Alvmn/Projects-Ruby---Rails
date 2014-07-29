6b42d02c046439d88370bed6ab0fa4f219135779
vagrant <vagrant@ironhack>
2014-07-29 07:38:55 +0000

    Git updated

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ActiveRecord::Base
+	def self.iron_find(id)
+		project = where(id: id).first
+		unless project.present?
+			raise
+		ActiveRecord::RecordNotFound.new("Couldn't find proyects with id=#{id}")
+		end
+	project
+	end
 end
 
 # Lo de arriba es lo mismo que ésto
